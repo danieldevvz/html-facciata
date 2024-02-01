2ec391844feeb514294791cffbdb227570529fec
Daniel Virguez <danieldevvz@gmail.com>
2024-02-01 16:24:27 -0400

    Added form modal client

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 angular.module('myApp', ['ngFileUpload'])
 .controller('MyCtrl', ['Upload', '$timeout', function (Upload, $timeout) {
     var vm = this;
+    vm.images = [];
 
     vm.uploadFiles = function(file) {
-        vm.f = file;
         if (file) {
             file.upload = Upload.upload({
                 url: 'http://localhost:3000/upload', // Cambia esta URL a la de tu servidor
@@ -13,7 +13,7 @@ angular.module('myApp', ['ngFileUpload'])
             file.upload.then(function (response) {
                 $timeout(function () {
                     file.result = response.data;
-                    vm.imageUrl = response.data.url; // Asumiendo que la respuesta contiene la URL de la imagen subida
+                    vm.images.push({url: response.data.url}); // Asumiendo que la respuesta contiene la URL de la imagen subida
                 });
             }, function (response) {
                 if (response.status > 0)
